23e4907b979a32d1a55bf7b05fa1f86c9d8861a9
ravi <ravi740136@gmail.com>
2024-04-19 23:32:13 +0530

    Multiclass file

---
+++ src/main/java/corejavatraining/nested/StringProcessorMulticlassMain.java
@@ -0,0 +1,8 @@
+package corejavatraining.nested;
+public class StringProcessorMulticlassMain {
+	public static void main(String[] args) {
+		StringFinder sf = new StringFinder("rajendran");
+		System.out.println(sf.findIndex("jen"));
+		}
+
+}
---
+++ src/main/java/corejavatraining/publicaccess/nested/StringProcessorMulticlassPublicMain.java
@@ -0,0 +1,9 @@
+package corejavatraining.publicaccess.nested;
+public class StringProcessorMulticlassPublicMain {
+	public static void main(String[] args) {
+		//can't be accessed outside the package
+		//StringFinder sf = new StringFinder("rajendran");
+		//System.out.println(sf.findIndex("jen"));
+		}
+
+}
